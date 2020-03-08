06f45ff82524e8eae0c88a3eaa146c7c7d97a895
Maxim Egorushkin <maxim.egorushkin@gmail.com>
2020-03-08 01:01:18 +0000

    Parallelize queries to different DNS.

--- README.md
+++ README.md
@@ -1,29 +1,51 @@
 # dns-benchmark
-DNS query response time benchmark.
+DNS query response latency benchmark.
+
+It measures the total time it takes to make DNS queries for Alexa top 1000 websites. The queries are done sequentially to measure the worst case latency rather than throughput.
+
+It uses [`dig`](https://en.wikipedia.org/wiki/Dig_(command) to make and time DNS queries to specific dns servers.
+
+There are normally a few layers of DNS caching, depending on one's network setup:
+
+* In the operating system.
+* In the router.
+* In ISP.
+
+A command line option allows to specify multiple DNS servers to
+
+multiple DNS server latencies the DNS servers should be arranged from the farthest to the closest one on the command line, so that a closer DNS doesn't make its parent DNS cached records.
 
 # Usage
 ```
 $ git clone https://github.com/max0x7ba/dns-benchmark.git
 $ cd dns-benchmark
+$ $ ./dns-benchmark.py -h
+usage: dns-benchmark.py [-h] [-n COUNT] [-s IP]
+
+Benchmark DNS query time.
+
+optional arguments:
+  -h, --help            show this help message and exit
+  -n COUNT, --count COUNT
+                        The number of requests to make, 0 means no limit. Default is 1000.
+  -s IP, --dns IP       A comma-separated list of DNS server IP addresses. Default is system.
+```
+
+In the following example I measure the latencies of Google (8.8.8.8), Cloudflare (1.1.1.1), ISP (81.139.57.100), router (192.168.50.1) and system DNSs:
+
+```
+$ ./dns-benchmark.py --dns 8.8.8.8,1.1.1.1,81.139.57.100
+It may take minutes, please wait...
+            dns,    time,    queries,     errors
+        8.8.8.8,  71.601,       1000,         16
+        1.1.1.1,  24.080,       1000,         16
+  81.139.57.100, 130.268,       1000,         16
+$ ./dns-benchmark.py --dns 192.168.50.1
+It may take minutes, please wait...
+            dns,    time,    queries,     errors
+   192.168.50.1,  42.669,       1000,         16
 $ ./dns-benchmark.py
-Report for system default dns:
-	queries: 1000
-	errors: 16
-	total time dig: 102.265 seconds
-	total time: 110.437 seconds
-
-$ ./dns-benchmark.py --dns 8.8.8.8
-Report for dns 8.8.8.8:
-	queries: 1000
-	errors: 16
-	total time dig: 57.918 seconds
-	total time: 66.291 seconds
-
-
-$ ./dns-benchmark.py --dns 1.1.1.1
-Report for dns 1.1.1.1:
-	queries: 1000
-	errors: 16
-	total time dig: 27.971 seconds
-	total time: 36.170 seconds
+It may take minutes, please wait...
+            dns,    time,    queries,     errors
+         system,  24.740,       1000,         16
 ```
--- dns-benchmark.py
+++ dns-benchmark.py
@@ -1,19 +1,29 @@
 #!/usr/bin/env python3
+import sys, os, re, time
+from argparse import ArgumentParser
+from tempfile import NamedTemporaryFile
+from zipfile import ZipFile
+from multiprocessing import Pool
 
-import argparse, sys, os, re, time
+
+encoding = "utf-8"
+
 
 def parse_alexa_top_csv(f, n):
 	for line in f:
-		domain = line[line.index(",") + 1:].strip()
+		line = line.decode(encoding)
+		domain = line[line.index(",") + 1:].strip() # Extract the 2nd column of the csv only.
 		yield domain
-		if n:
+		if n > 0:
 			n -= 1
 			if not n:
 				break
 
+
+re_dig_answer_count = re.compile(", ANSWER: (\d+),")
+re_query_time = re.compile(";; Query time: (\d+) msec")
+
 def parse_dig_output(f):
-	re_dig_answer_count = re.compile(", ANSWER: (\d+),")
-	re_query_time = re.compile(";; Query time: (\d+) msec")
 	for line in f:
 		m = re_dig_answer_count.search(line)
 		if m:
@@ -22,50 +32,76 @@ def parse_dig_output(f):
 		m = re_query_time.match(line)
 		if m:
 			msec = int(m.group(1))
-			return answer_count, msec
+			yield answer_count, msec
 
-	raise RuntimeError("Failed to parse dig output.");
 
-def main():
-	parser = argparse.ArgumentParser(description="Benchmark DNS query time.")
-	parser.add_argument("-n", "--count", type=int, default=1000, help="The number of requests to make, 0 means unlimited. Default is %(default)s.")
-	parser.add_argument("-s", "--dns", help="DNS server to use, default is the system default one.")
-	parser.add_argument("-v", "--verbose", default=False, action="store_true", help="Increase logging verbosity.")
-	args = parser.parse_args()
+def write_all(f, data):
+	data = data.encode(encoding)
+	while True:
+		written = f.write(data)
+		if written == len(data):
+			break
+		data = data[written:]
 
-	alexa_top_csv = "top-1m.csv"
-	if not os.access("top-1m.csv", os.R_OK):
-		os.system("wget -O top-1m.csv.zip http://s3.amazonaws.com/alexa-static/top-1m.csv.zip && unzip top-1m.csv.zip && rm top-1m.csv.zip")
 
-	dig = "/usr/bin/dig "
-	if args.dns:
-		dig += "@" + args.dns + " "
+dig = "/usr/bin/dig"
+
+def benchmark_dns(args):
+	dns, domains_file = args
+
+	dig_cmd = dig
+	if dns != "system":
+		dig_cmd += " @" + dns
+	dig_cmd += " -f " + domains_file
 
 	errors = 0
-	total_msec = 0
+	dig_msec = 0
 	count = 0
-	t0 = time.clock_gettime(time.CLOCK_MONOTONIC_RAW)
-	for domain in parse_alexa_top_csv(open(alexa_top_csv), args.count):
-		if args.verbose:
-			print(domain, end=" ... ")
-		dig_proc = os.popen(dig + domain)
-		answer_count, msec = parse_dig_output(dig_proc)
-		errors += not answer_count
-		total_msec += msec
-		assert not dig_proc.close()
-		count += 1
-		if args.verbose:
-			print(msec, "msec")
-	t1 = time.clock_gettime(time.CLOCK_MONOTONIC_RAW)
 
-	if args.dns:
-		print("Report for dns", args.dns + ":")
-	else:
-		print("Report for system default dns:")
-	print("\tqueries:", count)
-	print("\terrors:", errors)
-	print("\ttotal time dig: {:.3f} seconds".format(total_msec / 1000))
-	print("\ttotal time: {:.3f} seconds".format(t1 - t0))
+	t0 = time.clock_gettime(time.CLOCK_MONOTONIC_RAW)
+	dig_proc = os.popen(dig_cmd)
+	for answer_count, msec in parse_dig_output(dig_proc):
+		errors += not answer_count
+		dig_msec += msec
+		count += 1
+	t1 = time.clock_gettime(time.CLOCK_MONOTONIC_RAW)
+	dig_rc = dig_proc.close()
+	if dig_rc:
+		print("{}: dig terminated with {}.".format(dns, dig_rc), file=sys.stderr)
+
+	return count, errors, dig_msec, t1 - t0
+
+
+def main():
+	parser = ArgumentParser(description="Benchmark DNS query time.")
+	parser.add_argument("-n", "--count", type=int, default=1000,
+						help="The number of requests to make, 0 means no limit. Default is %(default)s.")
+	parser.add_argument("-s", "--dns", default="system", metavar="IP",
+						help="A comma-separated list of DNS server IP addresses. Default is %(default)s.")
+	args = parser.parse_args()
+
+	# Download Alexa top websites csv.
+	alexa_top_csv = "top-1m.csv.zip"
+	if not os.access(alexa_top_csv, os.R_OK):
+		os.system("wget -O {0} http://s3.amazonaws.com/alexa-static/{0}".format(alexa_top_csv))
+
+	if args.count >= 100:
+		print("It may take minutes, please wait...")
+
+	# To invoke dig only once per dns make a temporary file with all the domain names for dig to query.
+	with NamedTemporaryFile(buffering=(1024 * 1024)) as domains_file:
+		for domain in parse_alexa_top_csv(ZipFile(alexa_top_csv).open(alexa_top_csv.rstrip(".zip")), args.count):
+			write_all(domains_file, domain + '\n')
+		domains_file.flush()
+
+		# Run benchmarks for different dns in parallel.
+		dnss = [s.strip() for s in args.dns.split(',')]
+		results = Pool(len(dnss)).map(benchmark_dns, zip(dnss, [domains_file.name] * len(dnss)))
+
+	print("            dns,    time,    queries,     errors")
+	for dns, (count, errors, dig_msec, total_sec) in zip(dnss, results):
+		print("{:>15s}, {:7.3f}, {:10}, {:10}".format(dns, dig_msec * 1e-3, count, errors))
+
 
 if __name__ == "__main__":
 	main()
