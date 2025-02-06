af1c47cb6989ed84ff32240a65adfb8626d66833
vr-ski <vr.ski@yahoo.com>
2025-02-07 00:30:01 +0200

    replace subprocess with shutil

--- dns-benchmark.py
+++ dns-benchmark.py
@@ -4,7 +4,7 @@
 # Copyright (c) 2020 Maxim Egorushkin. MIT License. See the full licence in file LICENSE.
 
 
-import sys, os, re, time, subprocess
+import sys, os, re, time, shutil
 import urllib.request
 import csv
 from argparse import ArgumentParser
@@ -12,8 +12,8 @@ from tempfile import NamedTemporaryFile
 from multiprocessing import Pool
 
 def get_dig_path():
-	cmd_result = subprocess.run(["which", "dig"], capture_output=True, text=True)
-	if cmd_result.returncode != 0:
+	cmd_result = shutil.which("dig")
+	if cmd_result == None:
 		raise OSError(cmd_result.stderr)
 	return cmd_result.stdout.strip()
 
